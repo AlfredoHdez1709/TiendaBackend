3e28ce4620298dcb691efb8e9cd33598f6073933
geek1709 <alfred.black1209@outlook.es>
2017-08-06 20:03:31 -0500

    listoas

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1a@0f$ap#wh4e0lny&16&$9ynh2lxh$e8)qnfvd-pw^gx5#4-$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -133,3 +133,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (  
     os.path.join(BASE_DIR, 'static'),
 )
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
